f89164aaef77c43f95c8c5fc578e3d4d1e9a3867
Vidya <vidya@unknown0019e3d3c374.gateway.2wire.net>
2013-03-01 23:03:02 -0800

    SSDP notifications

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var deviceNotifier = require('./discovery/deviceNotifier');
+
+deviceNotifier.startAdvertisements(3000);
+
+app.listen(8080);
+ 
+app.configure( function() {
+    console.log('Node listening on: ' + '/');
+});
+
+app.get('/description.xml', function(req, res) {
+	
+	//var body = 'Hello World';
+	//res.setHeader('Content-Type', 'text/plain');
+	//res.setHeader('Content-Length', body.length);
+	//res.end(body);
+
+	res.sendfile('./description/xml/devicedescr.xml');
+    console.log('Sent device description');
+});
---
+++ discovery/deviceNotifier.js
@@ -0,0 +1,48 @@
+// Device advertisement
+
+var DeviceNotifier = function()
+{
+	var self = this;
+
+	this.upnpNotifyMulticastAddress = '239.255.255.250';
+	this.upnpNotifyMulticastPort = '1900';
+
+
+	this.notifyString =	"NOTIFY * HTTP/1.1\r\n" +
+		"HOST: 239.255.255.250:1900\r\n" +
+		"CACHE-CONTROL: max-age = 100\r\n" +
+		"LOCATION: http://192.168.10.101:8080/description.xml\r\n" +
+		"NT: upnp:rootdevice\r\n" +
+		"NTS: ssdp:alive\r\n" +
+		"SERVER: Ubuntu/12.04 UPnP/1.0 node-media-server/0.3\r\n" +
+		"USN: uuid:1eecac01-b4f5-4da3-a6f4-4696034c9ea8::upnp:rootdevice\r\n\r\n";
+
+
+	this.startAdvertisements = function (interval) {	
+
+	
+		var dgram = require('dgram'); 
+		this.server = dgram.createSocket("udp4"); 
+		this.server.bind();
+		this.server.setBroadcast(true);
+		this.server.setMulticastTTL(128);
+		this.server.addMembership(this.upnpNotifyMulticastAddress); 
+
+		setInterval(this.multicastNotify, interval);
+	}
+
+	this.stopAdvertisements = function (interval) {
+
+
+	}
+
+
+	this.multicastNotify = function () {
+		var message = new Buffer(self.notifyString);
+		self.server.send(message, 0, message.length, self.upnpNotifyMulticastPort, self.upnpNotifyMulticastAddress);
+		//console.log("Sent :" + message);
+		//server.close();
+	}
+}
+ 
+module.exports = new DeviceNotifier();
