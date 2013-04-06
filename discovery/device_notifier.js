a8e92cb30609a4754900a61e94139ca18e2298b2
Vinod KG <kgvinod@gmail.com>
2013-04-05 17:44:54 -0700

    Full implementation of device and service notifications

--- discovery/device_notifier.js
+++ discovery/device_notifier.js
@@ -27,6 +27,7 @@ function DeviceNotifier () {
 	this.server = undefined;	
 	this.upnpNotifyMulticastAddress = '239.255.255.250';
 	this.upnpNotifyMulticastPort = '1900';
+	this.deviceNotifyString = [];
 }
 
 
@@ -49,24 +50,74 @@ DeviceNotifier.prototype.startAdvertisements = function (iface_name, interval) {
 	this.server.addMembership(this.upnpNotifyMulticastAddress); 	
 	
 	
-	this.notifyString =	upnp_consts.SSDP_START_LINE_NOTIFY + '\r\n' +
+	this.deviceNotifyString.push(upnp_consts.SSDP_START_LINE_NOTIFY + '\r\n' +
 		'HOST: ' + upnp_consts.SSDP_DISCOVERY_MCAST_ADDRESS + ':' + 
             upnp_consts.SSDP_DISCOVERY_MCAST_PORT + '\r\n' +
 		'CACHE-CONTROL: max-age = 100\r\n' +
 		'LOCATION: http://' + this.ipAddr + ':' + config.web_server_port + '/upnp/description.xml\r\n' +
-		'NT: upnp:rootdevice\r\n' +
-		'NTS: ssdp:alive\r\n' +
 		'SERVER: ' + config.server_name + '\r\n' +
-		'USN: uuid:' + config.root_uuid + '::upnp:rootdevice\r\n\r\n';
+		'NTS: ssdp:alive\r\n' +
+		'NT: upnp:rootdevice\r\n' +
+		'USN: uuid:' + config.root_uuid + '::upnp:rootdevice\r\n\r\n');		
+
+	
+	this.deviceNotifyString.push(upnp_consts.SSDP_START_LINE_NOTIFY + '\r\n' +
+		'HOST: ' + upnp_consts.SSDP_DISCOVERY_MCAST_ADDRESS + ':' + 
+            upnp_consts.SSDP_DISCOVERY_MCAST_PORT + '\r\n' +
+		'CACHE-CONTROL: max-age = 100\r\n' +
+		'LOCATION: http://' + this.ipAddr + ':' + config.web_server_port + '/upnp/description.xml\r\n' +
+		'SERVER: ' + config.server_name + '\r\n' +
+		'NTS: ssdp:alive\r\n' +
+		'NT: uuid:' + config.root_uuid  + '\r\n' +
+		'USN: uuid:' + config.root_uuid + '\r\n\r\n');	
+
+	
+	this.deviceNotifyString.push(upnp_consts.SSDP_START_LINE_NOTIFY + '\r\n' +
+		'HOST: ' + upnp_consts.SSDP_DISCOVERY_MCAST_ADDRESS + ':' + 
+            upnp_consts.SSDP_DISCOVERY_MCAST_PORT + '\r\n' +
+		'CACHE-CONTROL: max-age = 100\r\n' +
+		'LOCATION: http://' + this.ipAddr + ':' + config.web_server_port + '/upnp/description.xml\r\n' +
+		'SERVER: ' + config.server_name + '\r\n' +
+		'NTS: ssdp:alive\r\n' +
+		'NT: urn:schemas-upnp-org:device:MediaServer:1\r\n' +
+		'USN: uuid:' + config.root_uuid + '::urn:schemas-upnp-org:device:MediaServer:1\r\n\r\n');	
+
+	
+	this.deviceNotifyString.push(upnp_consts.SSDP_START_LINE_NOTIFY + '\r\n' +
+		'HOST: ' + upnp_consts.SSDP_DISCOVERY_MCAST_ADDRESS + ':' + 
+            upnp_consts.SSDP_DISCOVERY_MCAST_PORT + '\r\n' +
+		'CACHE-CONTROL: max-age = 100\r\n' +
+		'LOCATION: http://' + this.ipAddr + ':' + config.web_server_port + '/upnp/description.xml\r\n' +
+		'SERVER: ' + config.server_name + '\r\n' +
+		'NTS: ssdp:alive\r\n' +
+		'NT: urn:schemas-upnp-org:service:ConnectionManager:1\r\n' +
+		'USN: uuid:' + config.root_uuid + '::urn:schemas-upnp-org:service:ConnectionManager:1\r\n\r\n');
+	
+	this.deviceNotifyString.push(upnp_consts.SSDP_START_LINE_NOTIFY + '\r\n' +
+		'HOST: ' + upnp_consts.SSDP_DISCOVERY_MCAST_ADDRESS + ':' + 
+            upnp_consts.SSDP_DISCOVERY_MCAST_PORT + '\r\n' +
+		'CACHE-CONTROL: max-age = 100\r\n' +
+		'LOCATION: http://' + this.ipAddr + ':' + config.web_server_port + '/upnp/description.xml\r\n' +
+		'SERVER: ' + config.server_name + '\r\n' +
+		'NTS: ssdp:alive\r\n' +
+		'NT: urn:schemas-upnp-org:service:ContentDirectory:1\r\n' +
+		'USN: uuid:' + config.root_uuid + '::urn:schemas-upnp-org:service:ContentDirectory:3\r\n\r\n');
+
 	
 	var self = this;
 
 	var multicastNotify = function () {
-		var message = new Buffer(self.notifyString);
-		self.server.send(message, 0, message.length, self.upnpNotifyMulticastPort, self.upnpNotifyMulticastAddress);
-		//console.log("Sent device NOTIFY");
-		//console.log("Sent :" + message);
-		//server.close();
+		
+		var i;
+		
+		for (i=0; i < self.deviceNotifyString.length; i++)
+		{
+			var message = new Buffer(self.deviceNotifyString[i]);
+			self.server.send(message, 0, message.length, self.upnpNotifyMulticastPort, self.upnpNotifyMulticastAddress);
+			//console.log("Sent device NOTIFY");
+			//console.log("Sent :" + message);
+			//server.close();
+		}
 	}
 	
 	setInterval(multicastNotify, this.interval*1000);
